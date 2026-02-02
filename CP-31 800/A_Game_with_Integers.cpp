2fa86f81186c4883ebd48405ae8a15bf37e86238
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2026-02-02 17:35:13 +0530

    Initial commit

---
+++ CP-31 800/A_Game_with_Integers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+    int n;
+    cin >> n;
+    if(n%3==0)
+    {
+        cout << "Second" << endl;
+    }
+    else{
+        cout << "First" << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
